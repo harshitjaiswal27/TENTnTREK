2c9a39544323e44688a4bcf7bb2504d28773ab43
harshitjaiswal27 <jaiswalhk99@gmail.com>
2020-04-11 19:04:18 +0530

    adding new campground

--- app.js
+++ app.js
@@ -1,50 +1,56 @@
 var express = require("express");
-
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set("view engine","ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+var content = [
+    {
+        title : "Manhattan",
+        img   : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg"
+    },
+    {
+        title : "New York",
+        img   : "https://cdn.getyourguide.com/img/tour_img-1096032-146.jpg"
+    },
+    {
+        title : "Las Vegas",
+        img : "https://www.casino.org/news/wp-content/uploads/2020/01/LAs-Vegas.jpg"
+    },
+    {
+        title : "Manhattan",
+        img   : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg"
+    },
+    {
+        title : "New York",
+        img   : "https://cdn.getyourguide.com/img/tour_img-1096032-146.jpg"
+    },
+    {
+        title : "Las Vegas",
+        img : "https://www.casino.org/news/wp-content/uploads/2020/01/LAs-Vegas.jpg"
+    }
+];
+
 app.get("/",function(req,res){
     res.render("landing");
 });
 
-app.get("/campground",function(req,res){
-
-    var content = [
-        {
-            title : "Manhattan",
-            img   : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg"
-        },
-        {
-            title : "New York",
-            img   : "https://cdn.getyourguide.com/img/tour_img-1096032-146.jpg"
-        },
-        {
-            title : "Las Vegas",
-            img : "https://www.casino.org/news/wp-content/uploads/2020/01/LAs-Vegas.jpg"
-        },
-        {
-            title : "Manhattan",
-            img   : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/1200px-Above_Gotham.jpg"
-        },
-        {
-            title : "New York",
-            img   : "https://cdn.getyourguide.com/img/tour_img-1096032-146.jpg"
-        },
-        {
-            title : "Las Vegas",
-            img : "https://www.casino.org/news/wp-content/uploads/2020/01/LAs-Vegas.jpg"
-        }
-    ];
-    res.render("campground",{ content : content});
+app.get("/campgrounds",function(req,res){
+    res.render("campgrounds",{ content : content});
 });
 
-app.post("/campground",function(req,res){
-    res.send("You sent a post request");
+app.post("/campgrounds",function(req,res){
+    var title = req.body.title;
+    var img = req.body.image;
+    var newCampground = { title , img };
+    content.push(newCampground);
+    res.redirect("/campgrounds")
 });
 
-app.get("/campground/new",function(req,res){
+app.get("/campgrounds/new",function(req,res){
     res.render("newCampground.ejs");
 });
 
